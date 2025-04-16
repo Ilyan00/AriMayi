a76687390be7e7418484d3e968f71aa92ab0a9fc
Ilyan-Jude Bain-Trimbach <ilyanjudecour@gmail.com>
2025-04-16 18:04:04 +0100

    feat: card for candidate

---
+++ src/app/components/CandidateCard.js
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card } from "antd";
+const { Meta } = Card;
+
+export default function CandidateCard({ nom, prenom, email }) {
+  return (
+    <Card
+      style={{ width: 300 }}
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
+    >
+      <Meta
+        avatar={
+          <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+        }
+        title={`${prenom} ${nom}`}
+        description={email}
+      />
+    </Card>
+  );
+}
---
+++ src/app/listeCandidats/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import CandidateCard from "../components/CandidateCard";
+
+export default function Home() {
+  return (
+    <div>
+      <CandidateCard nom="Doe" prenom="John" email="john.doe@example.com" />
+    </div>
+  );
+}
